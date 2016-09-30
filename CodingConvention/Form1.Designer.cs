9b376e14b23c5467df6f7fc3eb1c75811b10a653
Minh Hoang-B56125 <B56125@freescale.com>
2016-09-30 09:23:51 +0700

    Update design GUI

--- CodingConvention/Form1.Designer.cs
+++ CodingConvention/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.rtbCode = new System.Windows.Forms.RichTextBox();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.lstFunctions = new System.Windows.Forms.ListBox();
-            this.tab1 = new System.Windows.Forms.TabControl();
+            this.tabCodingConvention = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.btnFormatCode = new System.Windows.Forms.Button();
             this.cbGenPExDRV = new System.Windows.Forms.CheckBox();
@@ -42,7 +42,7 @@
             this.cbCleanLstFuncs = new System.Windows.Forms.CheckBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tab1.SuspendLayout();
+            this.tabCodingConvention.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,16 +76,16 @@
             this.lstFunctions.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lstFunctions_DrawItem);
             this.lstFunctions.SelectedIndexChanged += new System.EventHandler(this.lstFunctions_SelectedIndexChanged);
             // 
-            // tab1
+            // tabCodingConvention
             // 
-            this.tab1.Controls.Add(this.tabPage1);
-            this.tab1.Controls.Add(this.tabPage2);
-            this.tab1.Controls.Add(this.tabPage3);
-            this.tab1.Location = new System.Drawing.Point(12, 12);
-            this.tab1.Name = "tab1";
-            this.tab1.SelectedIndex = 0;
-            this.tab1.Size = new System.Drawing.Size(1079, 553);
-            this.tab1.TabIndex = 3;
+            this.tabCodingConvention.Controls.Add(this.tabPage1);
+            this.tabCodingConvention.Controls.Add(this.tabPage2);
+            this.tabCodingConvention.Controls.Add(this.tabPage3);
+            this.tabCodingConvention.Location = new System.Drawing.Point(12, 12);
+            this.tabCodingConvention.Name = "tabCodingConvention";
+            this.tabCodingConvention.SelectedIndex = 0;
+            this.tabCodingConvention.Size = new System.Drawing.Size(1079, 553);
+            this.tabCodingConvention.TabIndex = 3;
             // 
             // tabPage1
             // 
@@ -102,9 +102,10 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tabPage1.Size = new System.Drawing.Size(1071, 527);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Check code and SRS";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // btnFormatCode
@@ -185,7 +186,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1071, 527);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Gen code";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
@@ -195,7 +196,7 @@
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage3.Size = new System.Drawing.Size(1071, 527);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Uml";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -203,10 +204,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1129, 577);
-            this.Controls.Add(this.tab1);
+            this.Controls.Add(this.tabCodingConvention);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tab1.ResumeLayout(false);
+            this.tabCodingConvention.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
@@ -217,7 +218,7 @@
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.RichTextBox rtbCode;
         private System.Windows.Forms.ListBox lstFunctions;
-        private System.Windows.Forms.TabControl tab1;
+        private System.Windows.Forms.TabControl tabCodingConvention;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
